4ee08489f66204e96eadfc80e804a64ea3ff8964
venkiz-code <venki.ak@outlook.com>
2024-08-23 21:44:27 +0530

    Add files via upload

---
+++ AD-ExportComputers.ps1
@@ -0,0 +1,13 @@
+﻿# Import Active Directory module
+Import-Module ActiveDirectory
+
+# Set the output file path
+$outputFile = "C:\ADComputers.csv"
+
+# Get all computer accounts from Active Directory
+$computerAccounts = Get-ADComputer -Filter *
+
+# Export the computer accounts to a CSV file
+$computerAccounts | Export-Csv -Path $outputFile -NoTypeInformation
+
+Write-Host "Computer accounts exported to $outputFile"
